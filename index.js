12f1dba1e8aadc349b0b2bd854a806b87853ae3d
Tony Atkins <duhrer@gmail.com>
2015-10-28 10:52:07 +0100

    Simplified master `index.js` for ease of inclusion in other packages.

--- index.js
+++ index.js
@@ -7,15 +7,3 @@ require("./src/js/server/lib/password.js");
 require("./src/js/server/lib/standaloneRenderer.js");
 
 require("./src/js/server/api.js");
-require("./src/js/server/current.js");
-require("./src/js/server/docs.js");
-require("./src/js/server/forgot.js");
-require("./src/js/server/login.js");
-require("./src/js/server/logout.js");
-// TODO:  Not yet implemented
-//require("./src/js/server/reset.js");
-require("./src/js/server/session.js");
-require("./src/js/server/signup.js");
-require("./src/js/server/verify.js");
-// TODO:  Not yet finished
-//require("./src/js/server/verify-resend.js");
